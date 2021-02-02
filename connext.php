85018c94ff49ca57df7e9823f3a5f2a129c0ac4d
Ewelina Augustynowicz <augustyunowicz.ewelina09@gmail.com>
2021-02-02 13:33:17 +0100

    ...

---
+++ connext.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "mysql-ewelina.alwaysdata.net";
+$username = "ewelina";
+$password = "Ewelina@123456";
+$dbname = "ewelina_ewelina";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) 
+{
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
