0d1de5b610f5ca718058f04c8873d95f29c9ecb6
ewelina-augustynowicz-09 <augustynowicz.ewelina09@gmail.com>
2021-05-09 21:09:24 +0200

    ....

--- bibl.php
+++ bibl.php
@@ -1,4 +1,5 @@
 <?php
+
 $servername = "mysql-ewelina.alwaysdata.net";
 $username = "ewelina";
 $password = "Ewelina@123456";
@@ -11,31 +12,35 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
+
 ?>
 
-<!DOCTYPE html>
 <html>
   <head>
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Ewelina Augustynowicz 2Ti gr2</title>
-    <link rel="stylesheet" href="../style.css" />
+    <title>Ewelina Augustynowicz 2Ti gr.2</title>
+    <link rel="stylesheet" href="style.css" />
   </head>
   <body>
-    <div class="container">
+  <div class="tekst">
+      <div class="container">
             <div class="item colorBlue">
         <h2>Ewelina Augustynowicz klasa 2Ti grupa 2 numer 1</h2>
-        <h1><b>Książki</b></h1> 
+        <h1><b>Strona główna</b></h1> 
+        <audio controls autoplay loop>
+        <source src="https://vgmsite.com/soundtracks/addams-family-values/exhvgxneho/02%20Addams%20Family%20Values.mp3" type="audio/mpeg">
+        </audio>
       </div> 
     <div class="item colorRed">
         <div class="nav">
           <h2> Menu:</h2>
           <ul>
-            <li><a href="https://github.com/SK-2019/php-sql-wprowadzenie-ewelina-augustynowicz-09">github</a></li>
+            <li><a class="nav_link" href="https://github.com/SK-2019/php-sql-wprowadzenie-ewelina-augustynowicz-09">github</a></li>
             <br>
-            <li><a class="nav_link" href="../index.php"> Strona głowna</a></li>
+            <li><a class="nav_link" href="index.php"> Strona głowna</a></li>
             <br>
             <li><a class="nav_link" href="../pracownicy/pracownicy.php"> Pracownicy </a></li>
             <br>
@@ -51,29 +56,29 @@ if (!$conn) {
             <br>
             <li><a class="nav_link" href="../pracownicy/daneDoBazy.php">Dane do bazy</a></li>
             <br>
-            <li><a class="nav_link" href="../pracownicy/petla.php">Pętla</a></li>
-            <br>
             <li><a class="nav_link" href="../pracownicy/delete.php">Delete</a></li>
             <br>
-            <li><a class="nav_link" href="biblioteka/ksiazki.php">Książki</a></li>
+            <li><a class="nav_link" href="../flexbox+menu/flexbox.html"> Flexbox + menu</a></li>
+            <br>
           </ul> 
       </div> 
       </div>
     
 <div class="item colorGreen">
-<?php
-$sql = ("SELECT * FROM bibliotekaAutor");
-echo("<h2>Aułtorzy</h2>");
-$result=$conn->query($sql);
-<form action="bazaWypo.php" method="POST">
-echo("<input type="<select name='Autor' id='id_autor'>"");
-while($row=$result->fetch_assoc())
-{
-echo("<option value=".$row["id_autor"]."</option><option vealu=>".$row["Autor"]."</option>");
-}
-echo("<input type='Submit' value='Autor'><br>");
-echo("</select>");
-?>
+  <h3> Hello :) </h3>
+  <div class="nav">
+          <h2> Menu:</h2>
+          <ul>
+            <li><a class="nav_link" href="wynajmsam.php"> Wynajem samochodów </a></li>
+            <br>
+            <li><a class="nav_link" href="schronisko.php"> Schronisko </a></li>
+            <br>
+            <li><a class="nav_link" href="bibl.php"> biblioteka </a></li>
+            <br>
+            <li><a class="nav_link" href=""> cztery </a></li>
+            <br>
+            <li><a class="nav_link" href=""> piec </a></li>
+          </ul> 
 
 <?php
 $sql = ("SELECT * FROM bibliotekaTytuł");
@@ -104,7 +109,8 @@ echo("</tr>");
 }
 echo("</table>");
 ?>
-      </div>
-    </div>
-    </div>
+
+</div>
+</div>
+</div>
 </html>
